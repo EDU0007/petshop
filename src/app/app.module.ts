ff1193274ced72ada44a132f0dc6986b5217df9b
Eduardo0007 <santanaeduardospf@gmail.com>
2021-01-13 17:06:10 -0300

    primeiro commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LoginPageComponent } from './pages/account/login-page/login-page.component';
+import { ResetPasswordPageComponent } from './pages/account/reset-password-page/reset-password-page.component';
+import { ProductsPageComponent } from './store/products-page/products-page.component';
+import { PetsPageComponent } from './pages/account/pets-page/pets-page.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginPageComponent,
+    ResetPasswordPageComponent,
+    LoginPageComponent,
+ 
+    ProductsPageComponent,
+    PetsPageComponent,
+    ProductsPageComponent
   ],
   imports: [
     BrowserModule,
